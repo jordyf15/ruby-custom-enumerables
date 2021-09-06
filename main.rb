274beedc5dbf04b32fe7fded26fefc1bcf35ce47
jordyf15 <jordyferdian88@gmail.com>
2021-09-06 09:16:50 +0700

    created my_each_with_index custom enumerable method

--- main.rb
+++ main.rb
@@ -4,10 +4,19 @@ module Enumerable
       yield self[index]
     end
   end
+  def my_each_with_index
+    self.size.times do |index|
+      yield self[index],index
+    end
+  end
 end
 
-puts "my_each vs each"
-numbers = [1,2,3,4,5]
-numbers.my_each {|item| p item}
-numbers.each {|item| p item}
+# puts "my_each vs each"
+# numbers = [1,2,3,4,5]
+# numbers.my_each {|item| p item}
+# numbers.each {|item| p item}
 
+puts "my_each_with_index vs each_with_index"
+numbers = [1,2,3,4,5]
+numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
+numbers.each_with_index {|value,index| p "#{index}: #{value}"}
