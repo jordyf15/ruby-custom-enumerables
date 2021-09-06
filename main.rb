b49dbc48a146326c546d626b770eb7bc26b6231e
jordyf15 <jordyferdian88@gmail.com>
2021-09-06 20:43:12 +0700

    finished my_map method

--- main.rb
+++ main.rb
@@ -96,9 +96,19 @@ module Enumerable
     count
   end
 
-  def my_map
-    return self.to_enum(:map) unless block_given?
+  def my_map arg_proc = nil
     new_arr = []
+    unless arg_proc == nil
+      self.size.times do |index|
+        if arg_proc.call self[index]
+          new_arr << self[index]
+        else
+          new_arr << nil
+        end
+      end
+      return new_arr
+    end
+    return self.to_enum(:map) unless block_given?
     self.size.times do |index|
       if yield self[index]
         new_arr << self[index]
@@ -121,120 +131,120 @@ module Enumerable
   end
 end
 
-# puts "my_each vs each"
-# numbers = [1,2,3,4,5]
-# p numbers.my_each {|item| p item}
-# p numbers.each {|item| p item}
-# p numbers.my_each
-# p numbers.each
-# puts "\n"
-# hashes = {a: 'a', b: 'b', c: 'c'}
-# p hashes.my_each {|key, value| puts "#{key}: #{value}"}
-# p hashes.each {|key, value| puts "#{key}: #{value}"}
-# p hashes.my_each
-# p hashes.each
-# puts "\n\n"
-
-
-# puts "my_each_with_index vs each_with_index"
-# numbers = [1,2,3,4,5]
-# p numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
-# p numbers.each_with_index {|value,index| p "#{index}: #{value}"}
-# p numbers.my_each_with_index
-# p numbers.each_with_index
-# puts "\n"
-# hashes = {a: 'a', b: 'b', c: 'c'}
-# p hashes.my_each_with_index {|value, index| p "#{index}: #{value}"}
-# p hashes.each_with_index {|value, index| p "#{index}: #{value}"}
-# p hashes.my_each_with_index
-# p hashes.each_with_index
-# puts "\n\n"
-
-# puts "my_select vs select"
-# numbers = [1,2,3,4,5]
-# p numbers.my_select {|value| value %2 == 0}
-# p numbers.select {|value| value %2 == 0}
-# p numbers.my_select {|value| value %2 != 0}
-# p numbers.select {|value| value %2 != 0}
-# p numbers.my_select
-# p numbers.select 
-# puts "\n"
-# hashes = {a: 1, b: 2, c: 3, d: 4, e: 5}
-# p hashes.my_select {|key, value| value %2 == 0}
-# p hashes.select {|key, value| value %2 == 0}
-# p hashes.my_select {|key, value| value %2 != 0}
-# p hashes.select {|key, value| value %2 != 0}
-# p hashes.my_select
-# p hashes.select 
-
-# puts "my_all? vs all?"
-# numbers = [1,2,3,4,5]
-# correct_numbers = [2,4,6,8,10]
-# p numbers.my_all? {|value| value %2 == 0}
-# p numbers.all? {|value| value %2 == 0}
-# puts "\n"
-# p correct_numbers.my_all? {|value| value %2 == 0}
-# p correct_numbers.all? {|value| value %2 == 0}
-# puts "\n"
-# p numbers.my_all?(Numeric)
-# p numbers.all?(Numeric)
-# puts "\n"
-# p numbers.my_all?
-# p numbers.all?
-# puts "\n\n"
-
-# puts "my_any? vs any?"
-# numbers = [1,2,3,4,5]
-# wrong_numbers = [1,3,5,7,9]
-# p numbers.my_any? {|value| value %2 == 0}
-# p numbers.any? {|value| value %2 == 0}
-# p wrong_numbers.my_any? {|value| value %2 == 0}
-# p wrong_numbers.any? {|value| value %2 == 0}
-# puts "\n"
-# p numbers.my_any?
-# p numbers.any?
-# p numbers.my_any?(Integer)
-# p numbers.any?(Integer)
-# p numbers.my_any?(String)
-# p numbers.any?(String)
-# puts "\n"
-# hashes = {a: 1, b: 2, c: 3, d: 4, e: 5}
-# p hashes.any?
-# p hashes.my_any?
-# p hashes.any?([:a, 1])
-# p hashes.my_any?([:a, 1])
-# p hashes.any?([:a, 2])
-# p hashes.my_any?([:a, 2])
-# p hashes.any?{|key, value| value<5}
-# p hashes.my_any?{|key, value| value<5}
-# p hashes.any?{|key, value| value>5}
-# p hashes.my_any?{|key, value| value>5}
-# puts "\n\n"
-
-# puts "my_none? vs none?"
-# numbers = [1,2,3,4,5]
-# wrong_numbers = [1,3,5,7,9]
-# p numbers.my_none? {|value| value %2 == 0}
-# p numbers.none? {|value| value %2 == 0}
-# p wrong_numbers.my_none? {|value| value %2 == 0}
-# p wrong_numbers.none? {|value| value %2 == 0}
-# p [].my_none?
-# p [].none?
-# p [nil, true].my_none?
-# p [nil, true].none?
-# p [].my_none?(Float)
-# p [].none?(Float)
+puts "my_each vs each"
+numbers = [1,2,3,4,5]
+p numbers.my_each {|item| p item}
+p numbers.each {|item| p item}
+p numbers.my_each
+p numbers.each
+puts "\n"
+hashes = {a: 'a', b: 'b', c: 'c'}
+p hashes.my_each {|key, value| puts "#{key}: #{value}"}
+p hashes.each {|key, value| puts "#{key}: #{value}"}
+p hashes.my_each
+p hashes.each
 puts "\n\n"
 
-# puts "my_count vs count"
-# ary = [1, 2, 4, 2]
-# p ary.count  
-# p ary.my_count                    
-# p ary.count(2)     
-# p ary.my_count(2)                         
-# p ary.count {|x| x%2 == 0}  
-# p ary.my_count {|x| x%2 == 0} 
-# puts "\n\n"
+
+puts "my_each_with_index vs each_with_index"
+numbers = [1,2,3,4,5]
+p numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
+p numbers.each_with_index {|value,index| p "#{index}: #{value}"}
+p numbers.my_each_with_index
+p numbers.each_with_index
+puts "\n"
+hashes = {a: 'a', b: 'b', c: 'c'}
+p hashes.my_each_with_index {|value, index| p "#{index}: #{value}"}
+p hashes.each_with_index {|value, index| p "#{index}: #{value}"}
+p hashes.my_each_with_index
+p hashes.each_with_index
+puts "\n\n"
+
+puts "my_select vs select"
+numbers = [1,2,3,4,5]
+p numbers.my_select {|value| value %2 == 0}
+p numbers.select {|value| value %2 == 0}
+p numbers.my_select {|value| value %2 != 0}
+p numbers.select {|value| value %2 != 0}
+p numbers.my_select
+p numbers.select 
+puts "\n"
+hashes = {a: 1, b: 2, c: 3, d: 4, e: 5}
+p hashes.my_select {|key, value| value %2 == 0}
+p hashes.select {|key, value| value %2 == 0}
+p hashes.my_select {|key, value| value %2 != 0}
+p hashes.select {|key, value| value %2 != 0}
+p hashes.my_select
+p hashes.select 
+
+puts "my_all? vs all?"
+numbers = [1,2,3,4,5]
+correct_numbers = [2,4,6,8,10]
+p numbers.my_all? {|value| value %2 == 0}
+p numbers.all? {|value| value %2 == 0}
+puts "\n"
+p correct_numbers.my_all? {|value| value %2 == 0}
+p correct_numbers.all? {|value| value %2 == 0}
+puts "\n"
+p numbers.my_all?(Numeric)
+p numbers.all?(Numeric)
+puts "\n"
+p numbers.my_all?
+p numbers.all?
+puts "\n\n"
+
+puts "my_any? vs any?"
+numbers = [1,2,3,4,5]
+wrong_numbers = [1,3,5,7,9]
+p numbers.my_any? {|value| value %2 == 0}
+p numbers.any? {|value| value %2 == 0}
+p wrong_numbers.my_any? {|value| value %2 == 0}
+p wrong_numbers.any? {|value| value %2 == 0}
+puts "\n"
+p numbers.my_any?
+p numbers.any?
+p numbers.my_any?(Integer)
+p numbers.any?(Integer)
+p numbers.my_any?(String)
+p numbers.any?(String)
+puts "\n"
+hashes = {a: 1, b: 2, c: 3, d: 4, e: 5}
+p hashes.any?
+p hashes.my_any?
+p hashes.any?([:a, 1])
+p hashes.my_any?([:a, 1])
+p hashes.any?([:a, 2])
+p hashes.my_any?([:a, 2])
+p hashes.any?{|key, value| value<5}
+p hashes.my_any?{|key, value| value<5}
+p hashes.any?{|key, value| value>5}
+p hashes.my_any?{|key, value| value>5}
+puts "\n\n"
+
+puts "my_none? vs none?"
+numbers = [1,2,3,4,5]
+wrong_numbers = [1,3,5,7,9]
+p numbers.my_none? {|value| value %2 == 0}
+p numbers.none? {|value| value %2 == 0}
+p wrong_numbers.my_none? {|value| value %2 == 0}
+p wrong_numbers.none? {|value| value %2 == 0}
+p [].my_none?
+p [].none?
+p [nil, true].my_none?
+p [nil, true].none?
+p [].my_none?(Float)
+p [].none?(Float)
+puts "\n\n"
+
+puts "my_count vs count"
+ary = [1, 2, 4, 2]
+p ary.count  
+p ary.my_count                    
+p ary.count(2)     
+p ary.my_count(2)                         
+p ary.count {|x| x%2 == 0}  
+p ary.my_count {|x| x%2 == 0} 
+puts "\n\n"
 
 puts "my_map vs map"
 numbers = [1,2,3,4,5]
@@ -244,9 +254,19 @@ p numbers.my_map {|value| value if value %2 == 0}
 p numbers.map {|value| value if value %2 == 0}
 p numbers.my_map
 p numbers.map 
+my_proc = Proc.new {|value|value}
+p numbers.my_map(my_proc)
+my_proc = Proc.new {|value| value if value %2 == 0}
+p numbers.my_map(my_proc)
 puts "\n\n"
 
-# puts "my_inject vs inject"
-# numbers = [1,2,3,4,5]
-# p numbers.my_inject {|sum, n| sum*n } 
-# p numbers.inject {|sum, n| sum*n}
+puts "my_inject vs inject"
+numbers = [1,2,3,4,5]
+p numbers.my_inject {|sum, n| sum*n } 
+p numbers.inject {|sum, n| sum*n}
+
+def multiply_els arr
+  arr.my_inject {|sum, n| sum*n}
+end
+
+p multiply_els([2,4,5])
