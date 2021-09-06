c5a4953b6351f490b8d4d7be1fb68f59146ff3f2
jordyf15 <jordyferdian88@gmail.com>
2021-09-06 09:22:24 +0700

    created my_select custom enumerable method

--- main.rb
+++ main.rb
@@ -9,6 +9,13 @@ module Enumerable
       yield self[index],index
     end
   end
+  def my_select
+    selected_result = []
+    self.size.times do |index|
+      selected_result << self[index] if yield self[index]
+    end
+    selected_result
+  end
 end
 
 # puts "my_each vs each"
@@ -16,7 +23,16 @@ end
 # numbers.my_each {|item| p item}
 # numbers.each {|item| p item}
 
-puts "my_each_with_index vs each_with_index"
+# puts "my_each_with_index vs each_with_index"
+# numbers = [1,2,3,4,5]
+# numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
+# numbers.each_with_index {|value,index| p "#{index}: #{value}"}
+
+puts "my_select vs select"
 numbers = [1,2,3,4,5]
-numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
-numbers.each_with_index {|value,index| p "#{index}: #{value}"}
+p numbers.my_select {|value| value %2 == 0}
+p numbers.select {|value| value %2 == 0}
+p numbers.my_select {|value| value %2 != 0}
+p numbers.select {|value| value %2 != 0}
+
+
