782846b40472115b99f69c4d3721d74868e9e44a
jordyf15 <jordyferdian88@gmail.com>
2021-09-06 09:39:47 +0700

    created my_all? custom enumerable method

--- main.rb
+++ main.rb
@@ -20,25 +20,42 @@ module Enumerable
     end
     selected_result
   end
+
+  def my_all?
+    all_result = true
+    self.size.times do |index|
+      all_result = false unless yield self[index]
+    end
+    all_result
+  end
 end
 
-puts "my_each vs each"
-numbers = [1,2,3,4,5]
-p numbers.my_each {|item| p item}
-p numbers.each {|item| p item}
-puts "\n\n"
+# puts "my_each vs each"
+# numbers = [1,2,3,4,5]
+# p numbers.my_each {|item| p item}
+# p numbers.each {|item| p item}
+# puts "\n\n"
 
-puts "my_each_with_index vs each_with_index"
-numbers = [1,2,3,4,5]
-p numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
-p numbers.each_with_index {|value,index| p "#{index}: #{value}"}
-puts "\n\n"
+# puts "my_each_with_index vs each_with_index"
+# numbers = [1,2,3,4,5]
+# p numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
+# p numbers.each_with_index {|value,index| p "#{index}: #{value}"}
+# puts "\n\n"
 
-puts "my_select vs select"
+# puts "my_select vs select"
+# numbers = [1,2,3,4,5]
+# p numbers.my_select {|value| value %2 == 0}
+# p numbers.select {|value| value %2 == 0}
+# p numbers.my_select {|value| value %2 != 0}
+# p numbers.select {|value| value %2 != 0}
+# puts "\n\n"
+
+puts "my_all? vs all?"
 numbers = [1,2,3,4,5]
-p numbers.my_select {|value| value %2 == 0}
-p numbers.select {|value| value %2 == 0}
-p numbers.my_select {|value| value %2 != 0}
-p numbers.select {|value| value %2 != 0}
+correct_numbers = [2,4,6,8,10]
+p numbers.my_all? {|value| value %2 == 0}
+p numbers.all? {|value| value %2 == 0}
+p correct_numbers.my_all? {|value| value %2 == 0}
+p correct_numbers.all? {|value| value %2 == 0}
 puts "\n\n"
 
