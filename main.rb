cec843263679a4a2560c9a6780945747761ced47
jordyf15 <jordyferdian88@gmail.com>
2021-09-06 09:31:34 +0700

    added return value for my_each and my_each_index and also added spaces between tests

--- main.rb
+++ main.rb
@@ -3,12 +3,16 @@ module Enumerable
     self.size.times do |index|
       yield self[index]
     end
+    self
   end
+
   def my_each_with_index
     self.size.times do |index|
       yield self[index],index
     end
+    self
   end
+
   def my_select
     selected_result = []
     self.size.times do |index|
@@ -18,15 +22,17 @@ module Enumerable
   end
 end
 
-# puts "my_each vs each"
-# numbers = [1,2,3,4,5]
-# numbers.my_each {|item| p item}
-# numbers.each {|item| p item}
+puts "my_each vs each"
+numbers = [1,2,3,4,5]
+p numbers.my_each {|item| p item}
+p numbers.each {|item| p item}
+puts "\n\n"
 
-# puts "my_each_with_index vs each_with_index"
-# numbers = [1,2,3,4,5]
-# numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
-# numbers.each_with_index {|value,index| p "#{index}: #{value}"}
+puts "my_each_with_index vs each_with_index"
+numbers = [1,2,3,4,5]
+p numbers.my_each_with_index {|value, index| p "#{index}: #{value}"}
+p numbers.each_with_index {|value,index| p "#{index}: #{value}"}
+puts "\n\n"
 
 puts "my_select vs select"
 numbers = [1,2,3,4,5]
@@ -34,5 +40,5 @@ p numbers.my_select {|value| value %2 == 0}
 p numbers.select {|value| value %2 == 0}
 p numbers.my_select {|value| value %2 != 0}
 p numbers.select {|value| value %2 != 0}
-
+puts "\n\n"
 
